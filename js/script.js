b229cac0ca1c9dd38a726543eccf51580e8837ba
MD2020m <dmarshall2019@gmail.com>
2025-10-09 22:45:00 -0500

    Apply basic implementation of National Weather Service API to add live weather forecasting to weather.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
-const test_lat=30.2;
-const test_long=-90.4;
+// Load forcast data and add to weather.html
 
-const response = await fetch(`https://api.weather.gov/points/${test_lat},${test_long}`)
-console.log(response)
+// Declare and assign constants for National Weather Service API call
+const ic_lat = 39.773;
+const ic_long = -90.229;
+const url = `https://api.weather.gov/points/${ic_lat},${ic_long}`
+
+// async function to fetch data from National Weather Service API
+async function fetchForecast() {
+    // Call National Weather Service API
+    const response = await fetch(url);
+    const endpoints = await response.json();
+    // Navigate to URL for desired endpoint and request from that endpoint
+    const forecast_response = await fetch(endpoints.properties.forecast);
+    const forecast_data = await forecast_response.json();
+    const period_forecasts = await forecast_data.properties.periods;
+
+    // Retrieve data from forecast endpoint response
+    let names = [];
+    let detailed_forecasts = [];
+    period_forecasts.forEach(function(period) {
+        names.push(period.name);
+        detailed_forecasts.push(period.detailedForecast);
+    });
+
+    // Package forecast data as JSON
+    const forecast = {
+        "names": names,
+        "forecast": detailed_forecasts
+    };
+    return forecast;
+}
+
+// Define function to display weather forecast
+async function displayForecast() {
+    const forecastOutput = document.getElementById("weather-rprt-div");
+    const forecastData = await fetchForecast()
+    console.log(forecastData);
+
+    forecastOutput.innerHTML = "<ul>";
+    for (let i = 0; i < forecastData.names.length; i++) {
+        forecastOutput.innerHTML += `<li>${forecastData.names[i]}</li><p>${forecastData.forecast[i]}</p>`
+    }
+    forecastOutput.innerHTML += "</ul>"
+}
+
+displayForecast();
